65feb90f282ffc34773f4097dc388f2199041313
@Singh_Peeyush <43371031+Peeyush-kumar7656@users.noreply.github.com>
2021-05-19 14:27:37 +0530

    modify

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import '../styles/App.css';
 class App extends Component {
     render() {
         const relative_names=[
-            {id:"relativeListItem1", key:"Aakash Singh"},
-            {id:"relativeListItem2", key:"Ajay Singh"},
-            {id:"relativeListItem3", key:"Sweta Singh"},
-            {id:"relativeListItem4", key:"Tarun Singh"}
+            {id:"relativeListItem1", k:"Aakash Singh"},
+            {id:"relativeListItem2", k:"Ajay Singh"},
+            {id:"relativeListItem3", k:"Sweta Singh"},
+            {id:"relativeListItem4", k:"Tarun Singh"}
         ]
         const list_of_relatives=relative_names.map((name)=>
-        <li keys={name.id}>{name.key}</li>
+        <li key={name.id}>{name.k}</li>
         );
         return(
             <div id="main">
