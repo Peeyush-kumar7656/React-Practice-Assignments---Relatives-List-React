780c77eb0d0c255880b3f41890173db2d34bbec2
@Singh_Peeyush <43371031+Peeyush-kumar7656@users.noreply.github.com>
2021-05-19 14:22:26 +0530

    modify

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
 class App extends Component {
     render() {
-
+        const relative_names=[
+            {id:"relativeListItem1", key:"Aakash Singh"},
+            {id:"relativeListItem2", key:"Ajay Singh"},
+            {id:"relativeListItem3", key:"Sweta Singh"},
+            {id:"relativeListItem4", key:"Tarun Singh"}
+        ]
+        const list_of_relatives=relative_names.map((name)=>
+        <li keys={name.id}>{name.key}</li>
+        );
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+            <ol key={"relativeList"}>{list_of_relatives}</ol>
             </div>
         )
     }
 }
-
-
 export default App;
